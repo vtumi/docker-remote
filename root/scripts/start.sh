ce143945d8a280f244147d31f2a914a12f02c791
tumi <mphphy@gmail.com>
2025-11-13 11:35:19 +0800

    Initial commit

--- root/scripts/start.sh
+++ root/scripts/start.sh
@@ -1,5 +1,19 @@
 #!/bin/bash
 
+if [ ! -f /config/.config/openbox/menu.xml ] || ! cmp /defaults/menu.xml /config/.config/openbox/menu.xml; then
+    mkdir -p /config/.config/openbox
+    cp /defaults/menu.xml /config/.config/openbox/menu.xml
+    openbox --reconfigure
+fi
+
+if [ ! -f /config/.config/tint2/tint2rc ] then
+    mkdir -p /config/.config/tint2
+    cp /etc/xdg/tint2/tint2rc /config/.config/tint2/tint2rc
+    sed -i 's/^panel_items = .*/panel_items = T/' /config/.config/tint2/tint2rc
+    sed -i 's/^panel_position = .*/panel_position = top center horizontal/' /config/.config/tint2/tint2rc
+    sed -i 's/^taskbar_name = .*/taskbar_name = 0/' /config/.config/tint2/tint2rc
+fi
+
 nohup tint2 > /dev/null 2>&1 &
 
 if [[ -n "$PUID" && -n "$PGID" ]]; then
